c86a61837ef96d9bdc7c65a105efdc1afaf2fa11
Delgado <tristandelgado44@gmail.com>
2023-11-16 16:09:07 -0600

    Updated directory

--- src/main/java/game/minesweeper/NumberSprites.java
+++ src/main/java/game/minesweeper/NumberSprites.java
@@ -1,29 +1,28 @@
 package game.minesweeper;
 
 import javafx.scene.image.Image;
-
 public class NumberSprites {
-    Image zeroSprite  = new Image("C:\\MineSweeperProj\\MineSweeper\\Images\\ZeroSprite.png");
-    Image oneSprite   = new Image("C:\\MineSweeperProj\\MineSweeper\\Images\\OneSprite.png");
-    Image twoSprite   = new Image("C:\\MineSweeperProj\\MineSweeper\\Images\\TwoSprite.png");
-    Image threeSprite = new Image("C:\\MineSweeperProj\\MineSweeper\\Images\\ThreeSprite.png");
-    Image fourSprite  = new Image("C:\\MineSweeperProj\\MineSweeper\\Images\\FourSprite.png");
-    Image fiveSprite  = new Image("C:\\MineSweeperProj\\MineSweeper\\Images\\FiveSprite.png");
-    Image sixSprite   = new Image("C:\\MineSweeperProj\\MineSweeper\\Images\\SixSprite.png");
-    Image sevenSprite = new Image("C:\\MineSweeperProj\\MineSweeper\\Images\\SevenSprite.png");
-    Image eightSprite = new Image("C:\\MineSweeperProj\\MineSweeper\\Images\\EightSprite.png");
-    Image nineSprite  = new Image("C:\\MineSweeperProj\\MineSweeper\\Images\\NineSprite.png");
+    Image zeroSprite;
+    Image oneSprite;
+    Image twoSprite;
+    Image threeSprite;
+    Image fourSprite;
+    Image fiveSprite;
+    Image sixSprite;
+    Image sevenSprite;
+    Image eightSprite;
+    Image nineSprite;
     NumberSprites()
     {
-        Image zeroSprite  = new Image("C:\\MineSweeperProj\\MineSweeper\\Images\\ZeroSprite.png");
-        Image oneSprite   = new Image("C:\\MineSweeperProj\\MineSweeper\\Images\\OneSprite.png");
-        Image twoSprite   = new Image("C:\\MineSweeperProj\\MineSweeper\\Images\\TwoSprite.png");
-        Image threeSprite = new Image("C:\\MineSweeperProj\\MineSweeper\\Images\\ThreeSprite.png");
-        Image fourSprite  = new Image("C:\\MineSweeperProj\\MineSweeper\\Images\\FourSprite.png");
-        Image fiveSprite  = new Image("C:\\MineSweeperProj\\MineSweeper\\Images\\FiveSprite.png");
-        Image sixSprite   = new Image("C:\\MineSweeperProj\\MineSweeper\\Images\\SixSprite.png");
-        Image sevenSprite = new Image("C:\\MineSweeperProj\\MineSweeper\\Images\\SevenSprite.png");
-        Image eightSprite = new Image("C:\\MineSweeperProj\\MineSweeper\\Images\\EightSprite.png");
-        Image nineSprite  = new Image("C:\\MineSweeperProj\\MineSweeper\\Images\\NineSprite.png");
+        zeroSprite  = new Image("C:\\MineSweeperProj\\MineSweeper\\Images\\ZeroSprite.png");
+        oneSprite   = new Image("C:\\MineSweeperProj\\MineSweeper\\Images\\OneSprite.png");
+        twoSprite   = new Image("C:\\MineSweeperProj\\MineSweeper\\Images\\TwoSprite.png");
+        threeSprite = new Image("C:\\MineSweeperProj\\MineSweeper\\Images\\ThreeSprite.png");
+        fourSprite  = new Image("C:\\MineSweeperProj\\MineSweeper\\Images\\FourSprite.png");
+        fiveSprite  = new Image("C:\\MineSweeperProj\\MineSweeper\\Images\\FiveSprite.png");
+        sixSprite   = new Image("C:\\MineSweeperProj\\MineSweeper\\Images\\SixSprite.png");
+        sevenSprite = new Image("C:\\MineSweeperProj\\MineSweeper\\Images\\SevenSprite.png");
+        eightSprite = new Image("C:\\MineSweeperProj\\MineSweeper\\Images\\EightSprite.png");
+        nineSprite  = new Image("C:\\MineSweeperProj\\MineSweeper\\Images\\NineSprite.png");
     }
 }
