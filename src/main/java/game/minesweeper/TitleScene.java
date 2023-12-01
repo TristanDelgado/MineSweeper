b0f06c1cf299bb03b7a7d61cde8945c1c5b481e7
Delgado <tristandelgado44@gmail.com>
2023-11-30 18:26:54 -0600

    N/A

--- src/main/java/game/minesweeper/TitleScene.java
+++ src/main/java/game/minesweeper/TitleScene.java
@@ -1,33 +1,17 @@
 package game.minesweeper;
 
-//Instead of doing each screen as a group I will be doing each scene as
-//a scene.
-
-import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.layout.Background;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
-import javafx.event.ActionEvent;
-import javafx.application.Application;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.shape.Line;
-import javafx.scene.Group;
 import javafx.scene.paint.Color;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.shape.Circle;
-import javafx.stage.Window;
 
 public class TitleScene{
-    public Scene intro;
-    Group introGroup;
     public Button startGameButton;
-    public Button creditsButton;
+    public Group introGroup;
+    public Scene intro;
     TitleScene(double xWindowWidth, double yWindowWidth)
     {
         Text gameTitle = new Text("MineSweeper");
@@ -53,7 +37,7 @@ public class TitleScene{
         startGameButton.setLayoutY(yWindowWidth - (yWindowWidth * (2.0/3.0)));
 
         introGroup = new Group(gameTitle, credits, startGameButton);
-        intro = new Scene(introGroup, xWindowWidth, yWindowWidth, Color.GRAY);
+        intro      = new Scene(introGroup, xWindowWidth, yWindowWidth, Color.GRAY);
     }
 
     public Scene getScene(){return intro;}
